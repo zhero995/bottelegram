cbe842893447032151472a2d0c1b43ef2e2af680
Kristian Salavarrieta <krisalex995@gmail.com>
2025-07-14 19:36:00 -0500

    Archivo corregido: tracker.py con indentación válida

--- utils/tracker.py
+++ utils/tracker.py
@@ -28,10 +28,9 @@ async def check_inactive_users(context):
 
     data = load_data()
     if user_id not in data or not data[user_id].get("active"):
-       await bot.send_message(chat_id, 
-    f"El usuario @{job_data['username']} no compartió ningún archivo. Será eliminado.\n"
-    f"The user @{job_data['username']} did not share any file and will be removed.")
-
+        await bot.send_message(chat_id, 
+            f"El usuario @{job_data['username']} no compartió ningún archivo. Será eliminado.\n"
+            f"The user @{job_data['username']} did not share any file and will be removed.")
         await bot.ban_chat_member(chat_id, int(user_id))
         await bot.unban_chat_member(chat_id, int(user_id))
         await notify_expulsion(bot, job_data['username'])
